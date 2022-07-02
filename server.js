25ebbca1d1f030e8ee77edbf8fb7858a833306eb
Bandit Silachai <mbandit001@yahoo.co.th>
2022-07-02 11:38:48 +0700

    fix express

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
-var express = require('express');
-var jsonServer = require('json-server');
+const jsonServer = require('json-server');
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 3000;
+const express = require('express')
+
 var server = express();
 
-server.get('/', (req, res) => {
-    res.send("<h1>Hello</h1>")
-})
+server.use('/api', jsonServer.router('db.json'));
 
-server.use('/api', jsonServer.defaults(), jsonServer.router('db.json'));
 
-server.listen(3000);
+server.use(middlewares);
+server.use(router);
+
+server.listen(port);
